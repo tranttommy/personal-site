6f74bcf3bb374e0cf64cae0998c0206b3924e03e
Tommy Tran <tranttommy95@gmail.com>
2019-07-07 20:41:29 -0700

    put Logo component through styles

--- src/components/splash/Logo.js
+++ src/components/splash/Logo.js
@@ -1,19 +1,133 @@
 import React from 'react';
-import LogoDiv from './styles-logo';
+import PropTypes from 'prop-types';
+import styled, { keyframes } from 'styled-components';
+import { fadeIn, shouldAnimate, untransform } from '../styles';
+import { themeWhite } from '../styles-theme-colors';
 
-export default function Logo(props) {
-  return (
-    <LogoDiv {...props}>
+const Logo = ({ className }) => (
+  <div className={className}>
+    <div>
       <div>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
-    </LogoDiv>
-  );
-}
+    </div>
+  </div>
+);
+
+Logo.propTypes = {
+  className: PropTypes.string
+};
+
+const borderAppear = keyframes`
+  to {
+    padding: 25px;
+    border: 10px solid white;
+  }
+`;
+
+export default styled(Logo)`
+  height: 420px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  > div {
+    padding: 50px;
+    border: 15px solid transparent;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation: ${borderAppear} ease-in ${shouldAnimate('1s 2s')} forwards;
+
+    > div {
+      position: relative;
+      width: 350px;
+      height: 350px;
+
+      div {
+        position: absolute;
+        right: 0;
+        bottom: 0;
+
+        &:nth-of-type(odd) {
+          border-top: 50px solid ${themeWhite};
+        }
+
+        &:nth-of-type(even) {
+          border-left: 50px solid ${themeWhite};
+        }
+
+        &:nth-child(1) {
+          width: 350px;
+          height: 350px;
+          transform: translateX(250px);
+          opacity: 0;
+          animation:
+            ${fadeIn} ease-in ${shouldAnimate('1s')} forwards,
+            ${untransform} ease-in ${shouldAnimate('1s')} forwards
+          ;
+        }
+
+        &:nth-child(2) {
+          width: 300px;
+          height: 300px;
+          transform: translateY(250px);
+          opacity: 0;
+          animation:
+            ${fadeIn} ease-in ${shouldAnimate('1s .25s')} forwards,
+            ${untransform} ease-in ${shouldAnimate('1s .25s')} forwards
+          ;
+        }
+
+        &:nth-child(3) {
+          width: 250px;
+          height: 250px;
+          transform: translateX(250px);
+          opacity: 0;
+          animation:
+            ${fadeIn} ease-in ${shouldAnimate('1s .45s')} forwards,
+            ${untransform} ease-in ${shouldAnimate('1s .45s')} forwards
+          ;
+        }
+
+        &:nth-child(4) {
+          width: 200px;
+          height: 200px;
+          transform: translateY(250px);
+          opacity: 0;
+          animation:
+            ${fadeIn} ease-in ${shouldAnimate('1s .60s')} forwards,
+            ${untransform} ease-in ${shouldAnimate('1s .60s')} forwards
+          ;
+        }
+
+        &:nth-child(5) {
+          width: 150px;
+          height: 150px;
+          transform: translateX(250px);
+          opacity: 0;
+          animation:
+            ${fadeIn} ease-in ${shouldAnimate('1s .70s')} forwards,
+            ${untransform} ease-in ${shouldAnimate('1s .70s')} forwards
+          ;
+        }
+
+        &:nth-child(6) {
+          width: 100px;
+          height: 100px;
+          transform: translateY(250px);
+          opacity: 0;
+          animation:
+            ${fadeIn} ease-in ${shouldAnimate('1s .75s')} forwards,
+            ${untransform} ease-in ${shouldAnimate('1s .75s')} forwards
+          ;
+        }
+      }
+    }
+  }
+`;
