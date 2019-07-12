2743ff43f27d3ea9ea050e895c2db25c92b8c59a
Tommy Tran <tranttommy95@gmail.com>
2019-07-11 17:23:19 -0700

    changed colors

--- src/components/styles-theme-colors.js
+++ src/components/styles-theme-colors.js
@@ -3,5 +3,5 @@ export const themeGray = () => '#878787';
 export const themeWhite = () => '#FFFFFF';
 export const themeOrange = () => '#EBAF5A';
 export const themeBlack = () => '#191919';
-export const transparentBlack = () => 'rgba(25, 25, 25, .5)';
-export const transparentWhite = () => 'rgba(255, 255, 255, .5)';
+export const transparentBlack = () => 'rgba(25, 25, 25, .25)';
+export const transparentWhite = () => 'rgba(255, 255, 255, .75)';
